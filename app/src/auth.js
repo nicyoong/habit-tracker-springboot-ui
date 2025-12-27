65f937f5efe148edfb2390158b78ae6e77a52057
Nic <73386479+nicyoong@users.noreply.github.com>
2025-12-28 00:48:49 +0800

    auth js

---
+++ app/src/auth.js
@@ -0,0 +1,14 @@
+const KEY = "ht_token";
+
+export function setToken(token) {
+  localStorage.setItem(KEY, token);
+}
+export function getToken() {
+  return localStorage.getItem(KEY);
+}
+export function clearToken() {
+  localStorage.removeItem(KEY);
+}
+export function isAuthed() {
+  return !!getToken();
+}
